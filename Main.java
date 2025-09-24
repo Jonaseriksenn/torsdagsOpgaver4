855937202673464e59723f60d09981843f7c94f3
Jonaseriksenn <jonaseaslam@gmail.com>
2025-09-24 13:51:34 +0200

    Task 1

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+//1.a
+class Main{
+	public static void main (String [] args);
+
+//1.g
+	Team t1 = new Team ("Brøndby IF");
+//1.i
+	myTeam.setRank(1);
+	t1.addPlayer("Jonas");
+    t1.addPlayer("Morten");
+
+//1.k
+   	Team t2 = new Team("FC København");
+    t2.setRank(1);
+    t2.addPlayer("Lucas");
+    t2.addPlayer("Lars");
+
+    Team t3 = new Team("AGF");
+    t3.setRank(2);
+    t3.addPlayer("Emil");
+    t3.addPlayer("Patrick");
+
+    Team t4 = new Team("FC Nordsjælland");
+    t4.setRank(4);
+    t4.addPlayer("Nikolaj");
+    t4.addPlayer("Kevin");
+
+    Team t5 = new Team("Silkeborg IF");
+    t5.setRank(5);
+    t5.addPlayer("Mathias");
+    t5.addPlayer("Oliver");
+
+
+
+
+
+
+
+
+}
---
+++ Team.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+//1.b
+class Team{
+	private String teamName; 	//1.c
+	private int rank;		 	//1.d
+	private ArrayList<String> playerNames; //1.e
+
+//1.f	
+	public Team (String teamName){
+	this.teamName = name;
+	this.playerNames = ArrayList<String>(); 
+	}
+
+//1.h
+	public void setRank(int rank){
+
+	}
+
+//1.j
+	public String toString(){
+		return "Hold: " + name + ", Rank: "´+ rank + ", Players:" + players;
+	} 
+}
